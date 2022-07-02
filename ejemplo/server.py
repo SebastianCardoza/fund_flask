2c3b04e01a22c269f3dd4fd4dcaed292bdb3aba6
Sebastian Cardoza <jscboulangger@gmail.com>
2022-07-02 11:34:50 -0500

    tabla_html

--- ejemplo/server.py
+++ ejemplo/server.py
@@ -1,3 +1,4 @@
+from ast import Return
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -8,6 +9,16 @@ def index():
 @app.route("/hello/<string:banana>/<int:num>")
 def hello(banana, num):
     return render_template("hello.html",banana=banana, num=num)
+
+@app.route("/lists")
+def lists():
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template('lists.html', random_numbers=[3,1,5], students=student_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
